842c3484974891a92c8e47150fc3cbdb5e9ed784
Victor <staskefir99@gmail.com>
2019-03-08 11:47:53 +0200

    less5

---
+++ lesson 5/2 задание/src/js/tempCodeRunnerFile.js
@@ -0,0 +1,42 @@
+let start = document.getElementById('start'),
+    budgetValue = document.querySelectorAll('.budget-value'),
+    daybudgetValue =document.querySelectorAll('.daybudget-value'),
+    levelValue =document.querySelectorAll('.level-value'),
+    expensesValue = document.getElementsByClassName('expensesValue'),
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.querySelectorAll('.income-value'),
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearSavingsValue = document.querySelectorAll('.yearsavings-value'),
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    elementButtons = document.getElementsByTagName('button')[0],
+    elementButtons1 = document.getElementsByTagName('button')[1],
+    elementButtons2 = document.getElementsByTagName('button')[2],
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    savings = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent= document.querySelector('#percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+console.log(start);
+console.log(budgetValue);
+console.log(daybudgetValue);
+console.log(levelValue);
+console.log(optionalExpensesValue);
+console.log(incomeValue);
+console.log(monthSavingsValue);
+console.log(yearSavingsValue);
+console.log(expensesItem);
+console.log(elementButtons);
+console.log(elementButtons1);
+console.log(elementButtons2);
+console.log(optionalexpensesItem);
+console.log(chooseIncome);
+console.log(savings);
+console.log(sum);
+console.log(percent);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
